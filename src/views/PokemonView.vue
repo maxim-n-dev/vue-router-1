fd37de16c73a1d98ba07938b8311c816bf7308a5
Maxim Nakonechnyy <maxim.n.dev@gmail.com>
2023-11-08 01:10:03 +0100

    First commit

---
+++ src/views/PokemonView.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+
+    const pokemons = ref([]);
+
+
+    onMounted(() => {
+        fetchPokemons();
+    });
+
+    const fetchPokemons = async () => {
+        try {
+            const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=200');
+            pokemons.value = await res.json();
+            console.log(JSON.stringify(pokemons.value.results))
+        } catch (error) {
+            console.log(error)
+        } finally {
+
+        }
+    }
+
+
+</script>
+
+<template>
+    <PokemonCard />
+</template>
